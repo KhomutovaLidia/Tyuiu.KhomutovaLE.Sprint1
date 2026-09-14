3871a280a8f8baf9d7f6405fab0e2cd90e68c240
Tyuiu.KhomutovaLE@outlook.com <dereminalila@gmail.com>
2026-09-14 18:13:00 +0500

    Add project files.

---
+++ Tyuiu.KhomutovaLE.Sprint1.Task0.V22.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KhomutovaLE.Sprint1.Task0.V22.Lib;
+
+namespace Tyuiu.KhomutovaLE.Sprint1.Task0.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidEx()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(10, res);
+        }
+    }
+}
---
+++ Tyuiu.KhomutovaLE.Sprint1.Task0.V22/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.KhomutovaLE.Sprint1.Task0.V22.Lib;
+
+namespace Tyuiu.KhomutovaLE.Sprint1.Task0.V22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Хомутова Л. Е. | РППб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #22                                                             *");
+            Console.WriteLine("* Выполнила: Хомутова Лидия Евгеньевна | РППб-26-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 6*2-(5-3)               *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 6*2-(5-3)                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadLine();
+        }
+    }
+}

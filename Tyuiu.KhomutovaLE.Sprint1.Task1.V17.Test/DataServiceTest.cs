6b2d8f955e3c91d85da75437937802def0fc58ac
Tyuiu.KhomutovaLE@outlook.com <dereminalila@gmail.com>
2026-09-14 21:16:43 +0500

    Task1

---
+++ Tyuiu.KhomutovaLE.Sprint1.Task1.V17.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.KhomutovaLE.Sprint1.Task1.V17.Lib;
+
+namespace Tyuiu.KhomutovaLE.Sprint1.Task1.V17.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidEx()
+        {
+            DataService ds = new DataService();
+            double x = 2.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(18, res);
+        }
+    }
+}
---
+++ Tyuiu.KhomutovaLE.Sprint1.Task1.V17/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.KhomutovaLE.Sprint1.Task1.V17.Lib;
+
+namespace Tyuiu.KhomutovaLE.Sprint1.Task1.V17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Хомутова Л. Е. | РППб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнила: Хомутова Лидия Евгеньевна | РППб-26-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле x*y*(5-x) и печатает его на экране.      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.WriteLine("Введите значение перемненной X:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение перемненной Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+
+            Console.ReadLine();
+        }
+    }
+}

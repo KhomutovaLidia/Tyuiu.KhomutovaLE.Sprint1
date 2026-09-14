2140ae4ac7e532d62753a9328460441c0874618b
Tyuiu.KhomutovaLE@outlook.com <dereminalila@gmail.com>
2026-09-14 22:10:55 +0500

    Task2

---
+++ Tyuiu.KhomutovaLE.Sprint1.Task2.V12/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.KhomutovaLE.Sprint1.Task2.V12.Lib;
+
+namespace Tyuiu.KhomutovaLE.Sprint1.Task2.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Хомутова Л. Е. | РППб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнила: Хомутова Лидия Евгеньевна | РППб-26-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x, y, z;
+
+            Console.WriteLine("Введите значение длины:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение ширины:");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение высоты:");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.CalculateParallelepipedVolume(x, y, z));
+
+            Console.ReadLine();
+        }
+    }
+}
